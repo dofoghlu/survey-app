76a9754a153c4c426ebcf49e391a3b99e63831df
dofoghlu <61784584+dofoghlu@users.noreply.github.com>
2025-11-30 16:30:09 +0000

    create folder structure and add storybook

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SurveyBuilder } from './features/survey/survey-builder/survey-builder';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: SurveyBuilder,
+    }
+];
---
+++ src/app/features/survey/survey-builder/survey-builder.stories.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from "@storybook/angular";
+import { SurveyBuilder } from "./survey-builder";
+
+const meta: Meta<SurveyBuilder> = {
+  component: SurveyBuilder,
+};
+
+export default meta;
+type Story = StoryObj<SurveyBuilder>;
+
+export const Default: Story = {
+};
