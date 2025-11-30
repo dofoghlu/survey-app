2ad321a42ce910c2841b73ca7fbf8f3727e2e5a7
dofoghlu <61784584+dofoghlu@users.noreply.github.com>
2025-11-30 17:11:56 +0000

    add survey form and add question functionality

--- src/app/features/survey/survey-builder/survey-builder.ts
+++ src/app/features/survey/survey-builder/survey-builder.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { QuestionEditor } from "../question-editor/question-editor";
+import { QuestionEditor } from '../question-editor/question-editor';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Question } from '../models/question';
+import { Survey } from '../models/survey';
 
 @Component({
   selector: 'app-survey-builder',
@@ -7,5 +10,46 @@ import { QuestionEditor } from "../question-editor/question-editor";
   templateUrl: './survey-builder.html',
 })
 export class SurveyBuilder {
+  surveyForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(survey?: Survey) {
+    this.surveyForm = this.fb.group({
+      id: [survey?.id || null],
+      title: [survey?.title || null, Validators.required],
+      description: [survey?.description || null],
+      questions: this.fb.array(survey?.questions?.map((q) => this.createQuestion(q)) || []),
+    });
+
+    if ((this.surveyForm.get('questions') as FormArray).length === 0) {
+      this.addQuestion();
+    }
+  }
+
+  createQuestion(question?: Question): FormGroup {
+    return this.fb.group({
+      questionId: [question?.questionId || 0],
+      questionText: [question?.questionText || null, Validators.required],
+      mandatoryInd: [question?.mandatoryInd || false],
+      questionType: [question?.questionType || 1],
+      options: [question?.options || []],
+      randomizeOptionsInd: [question?.randomizeOptionsInd || false],
+      cards: [question?.cards || []],
+      programmerNotes: [question?.programmerNotes || null],
+      instructions: [question?.instructions || null],
+    });
+  }
+
+  get questions(): FormArray {
+    return this.surveyForm.get('questions') as FormArray;
+  }
+
+  addQuestion() {
+    this.questions.push(this.createQuestion());
+  }
 }
